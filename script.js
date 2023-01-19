7c3b4b2229231219187ea26ba2462a9346b49866
Valetina-2608 <valusha2608@gmail.com>
2023-01-19 18:40:51 +0200

    Change function shortBreakChanges, add function longBreakChanges

--- script.js
+++ script.js
@@ -9,14 +9,32 @@ let fon2 = document.querySelector('.fon2');
 let fon3 = document.querySelector('.fon3');
 
 
+
 btn_short_break.addEventListener('click', shortBreakChanges);
 
 function shortBreakChanges(){
     fon1.style.display = 'none';
+    fon3.style.display = 'none';
     main.style.backgroundColor = '#45538B';
     fon2.style.display = 'block';
     fon2.style.backgroundColor = '#8B9BDC';
     btn_short_break.style.backgroundColor = '#6474B3';
     btn_long_break.style.backgroundColor = '#8B9BDC';
     btn_pomodoro.style.backgroundColor = '#8B9BDC';
+}
+
+
+
+
+btn_long_break.addEventListener('click', longBreakChanges);
+
+function longBreakChanges(){
+    fon1.style.display = 'none';
+    fon2.style.display = 'none';
+    fon3.style.display = 'block';
+    main.style.backgroundColor = '#1D7074';
+    fon3.style.backgroundColor = '#5DC8CD';
+    btn_short_break.style.backgroundColor = '#5DC8CD';
+    btn_long_break.style.backgroundColor = '#006064';
+    btn_pomodoro.style.backgroundColor = '#5DC8CD';
 }
