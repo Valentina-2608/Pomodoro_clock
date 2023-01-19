15fa7020d5dc35ec1752438d4ecaf8263a08657d
Valetina-2608 <valusha2608@gmail.com>
2023-01-19 15:36:39 +0200

    Add function changeFonSecond

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Script */
+
+let btn_pomodoro = document.getElementById('btn_pomodoro');
+let btn_short_break = document.getElementById('btn_short_break');
+let btn_long_break = document.getElementById('btn_long_break');
+let main = document.querySelector('.main');
+let fon1 = document.querySelector('.fon1');
+let fon2 = document.querySelector('.fon2');
+let fon3 = document.querySelector('.fon3');
+
+
+btn_short_break.addEventListener('click', changeFonSecond);
+function changeFonSecond(){
+    fon1.style.display = 'none';
+    main.style.backgroundColor = '#45538B';
+    fon2.style.display = 'block';
+    fon2.style.backgroundColor = '#8B9BDC';
+    btn_short_break.style.backgroundColor = '#6474B3';
+    btn_long_break.style.backgroundColor = '#8B9BDC';
+    btn_pomodoro.style.backgroundColor = '#8B9BDC';
+}
