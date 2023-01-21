623b2bdd905e0bda1d1bd182eb1e4cbfad738bf4
Valetina-2608 <valusha2608@gmail.com>
2023-01-21 18:53:24 +0200

    Add function startTimer2, change function startTimer1, pomodoroChanges, shortBreakChanges

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ let fon1 = document.querySelector('.fon1');
 let fon2 = document.querySelector('.fon2');
 let fon3 = document.querySelector('.fon3');
 
-
+let timer1 = document.querySelector('.timer1');
+let timer2 = document.querySelector('.timer2');
+let timer3 = document.querySelector('.timer3');
 
 btn_short_break.addEventListener('click', shortBreakChanges);
 
@@ -21,6 +23,11 @@ function shortBreakChanges(){
     btn_short_break.style.backgroundColor = '#6474B3';
     btn_long_break.style.backgroundColor = '#8B9BDC';
     btn_pomodoro.style.backgroundColor = '#8B9BDC';
+
+
+    timer1.style.display = 'none';
+    timer2.style.display = 'block';
+    timer3.style.display = 'none';
 }
 
 
@@ -53,18 +60,27 @@ function pomodoroChanges(){
     btn_short_break.style.backgroundColor ='#F3AD73';
     btn_long_break.style.backgroundColor = '#F3AD73';
     btn_pomodoro.style.backgroundColor = '#F39445';
+
+
+    timer2.style.display = 'none';
+    timer3.style.display = 'none';
+    timer1.style.display = 'block';
 }
 
 
 /* Timers */
 
 /* Timer Pomodoro */
+
+
+
 let start_pomodoro = document.getElementById('start_pomodoro');
 let pause_pomodoro = document.getElementById('pause_pomodoro');
 
 start_pomodoro.addEventListener('click', startTimer1);
 
 function startTimer1(){
+   
     let timer1_minutes = document.getElementById('timer1_minutes');
     let timer1_seconds = document.getElementById('timer1_seconds');
     let time1_start = Number(timer1_minutes.value) * 60 + Number(timer1_seconds.value);
@@ -73,7 +89,7 @@ function startTimer1(){
     let myAudio = document.querySelector('#audio')
 
 
-    let updateTime = setInterval(function(){
+    let updateTime1 = setInterval(function(){
         let minutes1_Changed = Math.floor(time1_start/60);
         let seconds1_Changed = time1_start % 60;
         if (seconds1_Changed < 10 ) {
@@ -106,11 +122,74 @@ function startTimer1(){
 
 
       
-    pause_pomodoro.addEventListener('click', pauseTimer);
+    pause_pomodoro.addEventListener('click', pauseTimer1);
    
-    function pauseTimer(){
+    function pauseTimer1(){
         start_pomodoro.style.display = 'block';
         pause_pomodoro.style.display = 'none';
-        clearInterval(updateTime);
+        clearInterval(updateTime1);
+    }
+}
+
+
+
+
+
+
+/* Timer Short Break */
+let start_short_break = document.getElementById('start_short_break');
+let pause_short_break = document.getElementById('pause_short_break');
+
+start_short_break.addEventListener('click', startTimer2);
+
+function startTimer2(){
+   
+    let timer2_minutes = document.getElementById('timer2_minutes');
+    let timer2_seconds = document.getElementById('timer2_seconds');
+    let time2_start = Number(timer2_minutes.value) * 60 + Number(timer2_seconds.value);
+    start_short_break.style.display = 'none';
+    pause_short_break.style.display = 'block';
+    let myAudio = document.querySelector('#audio')
+
+
+    let updateTime2 = setInterval(function(){
+        let minutes2_Changed = Math.floor(time2_start/60);
+        let seconds2_Changed = time2_start % 60;
+        if (seconds2_Changed < 10 ) {
+            timer2_seconds.value = '0'+ seconds2_Changed;
+        } else{
+            timer2_seconds.value = seconds2_Changed;
+        }
+        if (minutes2_Changed < 10 ) {
+            timer2_minutes.value =  '0'+ minutes2_Changed;
+        } else{
+            timer2_minutes.value =  minutes2_Changed;
+        }
+
+        if(timer2_minutes.value === '00' && timer2_seconds.value === '00'){
+
+            clearInterval(updateTime);
+            myAudio.play();
+            setTimeout(() =>{
+                start_short_break.style.display = 'block';
+                pause_short_break.style.display = 'none'; 
+                timer2_minutes.value = '5';
+                timer2_seconds.value = '00';
+               
+            }, 3000)   
+
+         }
+        time2_start--;
+
+    }, 1000);
+
+
+      
+    pause_short_break.addEventListener('click', pauseTimer2);
+   
+    function pauseTimer2(){
+        start_short_break.style.display = 'block';
+        pause_short_break.style.display = 'none';
+        clearInterval(updateTime2);
     }
 }
