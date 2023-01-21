42f0be21a4249f97c3f4226edd08a93c9fd591cf
Valetina-2608 <valusha2608@gmail.com>
2023-01-21 18:13:36 +0200

    Add function startTimer1

--- script.js
+++ script.js
@@ -54,3 +54,63 @@ function pomodoroChanges(){
     btn_long_break.style.backgroundColor = '#F3AD73';
     btn_pomodoro.style.backgroundColor = '#F39445';
 }
+
+
+/* Timers */
+
+/* Timer Pomodoro */
+let start_pomodoro = document.getElementById('start_pomodoro');
+let pause_pomodoro = document.getElementById('pause_pomodoro');
+
+start_pomodoro.addEventListener('click', startTimer1);
+
+function startTimer1(){
+    let timer1_minutes = document.getElementById('timer1_minutes');
+    let timer1_seconds = document.getElementById('timer1_seconds');
+    let time1_start = Number(timer1_minutes.value) * 60 + Number(timer1_seconds.value);
+    start_pomodoro.style.display = 'none';
+    pause_pomodoro.style.display = 'block';
+    let myAudio = document.querySelector('#audio')
+
+
+    let updateTime = setInterval(function(){
+        let minutes1_Changed = Math.floor(time1_start/60);
+        let seconds1_Changed = time1_start % 60;
+        if (seconds1_Changed < 10 ) {
+            timer1_seconds.value = '0'+ seconds1_Changed;
+        } else{
+            timer1_seconds.value = seconds1_Changed;
+        }
+        if (minutes1_Changed < 10 ) {
+            timer1_minutes.value =  '0'+ minutes1_Changed;
+        } else{
+            timer1_minutes.value =  minutes1_Changed;
+        }
+
+        if(timer1_minutes.value === '00' && timer1_seconds.value === '00'){
+
+            clearInterval(updateTime);
+            myAudio.play();
+            setTimeout(() =>{
+                start_pomodoro.style.display = 'block';
+                pause_pomodoro.style.display = 'none'; 
+                timer1_minutes.value = '25';
+                timer1_seconds.value = '00';
+               
+            }, 3000)   
+
+         }
+        time1_start--;
+
+    }, 1000);
+
+
+      
+    pause_pomodoro.addEventListener('click', pauseTimer);
+   
+    function pauseTimer(){
+        start_pomodoro.style.display = 'block';
+        pause_pomodoro.style.display = 'none';
+        clearInterval(updateTime);
+    }
+}
